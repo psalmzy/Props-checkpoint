d31e674c05a86b893bfda00b6814e2ee9b263c09
Psalmzy <psalmwelloladokun@gmail.com>
2023-06-21 20:54:49 +0100

    Props Checkpoint

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PlayersList from './PlayersList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <PlayersList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/players.js
@@ -0,0 +1,37 @@
+const players = [
+    {
+      name: 'Eden Hazard',
+      team: 'Real Madrid',
+      nationality: 'Belgian',
+      jerseyNumber: 10,
+      age: 32,
+      imageUrl: 'https://dailypost.ng/wp-content/uploads/2023/01/Eden-Hazard-scaled.jpeg',
+    },
+    {
+      name: 'Neymar JR',
+      team: 'PSG',
+      nationality: 'Brazilian',
+      jerseyNumber: 10,
+      age: 31,
+      imageUrl: 'https://talksport.com/wp-content/uploads/sites/5/2023/05/neymar-paris-saint-germain-looks-817730667.jpg',
+    },
+    {
+      name: 'Cristiano Ronaldo',
+      team: 'Al Nassr',
+      nationality: 'Potuguese',
+      jerseyNumber: 7,
+      age: 38,
+      imageUrl: 'https://a.espncdn.com/photo/2022/1130/r1099444_1296x729_16-9.jpg',  
+    },
+    {
+      name: 'Lionel Messi',
+      team: 'Inter Miami',
+      nationality: 'Argentine',
+      jerseyNumber: 10,
+      age: 36,
+      imageUrl: 'https://media.cnn.com/api/v1/images/stellar/prod/230615095313-lionel-messi-goal-china-file-061523.jpg?c=16x9&q=h_720,w_1280,c_fill',  
+    }
+  ];
+  
+  export default players;
+  
